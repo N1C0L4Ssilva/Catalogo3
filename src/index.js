dbdde03eba3ff4c8c01548e9d4d0bcb79ca382fc
N1C0L4Ssilva <nicolassipva3317@gmail.com>
2023-12-04 17:25:36 -0300

    Criação da pasta Classes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDoc, getDocs } from "firebase/firestore"
+// DB CONNECTION
+const {initializeApp}=require('firebase/app')
+const {getFirestore, collection, getDoc, getDocs, doc}=require('firebase/firestore')
 
 const FireApp=initializeApp({
   apiKey: "AIzaSyDDWfRoi1WMnFxfiO2Inu1usm7xZfyD-A0",
@@ -11,10 +11,16 @@ const FireApp=initializeApp({
   appId: "1:537589475837:web:1f34b8de238429279a9194",
   measurementId: "G-L22BT0Q7KM"
 });
-const db= getFirestore(FireApp)
-const Data=db.collection("Produto").getDoc()
-console.log(Data)
+const db=getFirestore(FireApp)
+const Docs=getDocs(collection(db,"PRODUTOS"))
+Docs.then((Value)=>{
+  Value.forEach((v)=>{
+    console.log(v.data())
+  })
+})
 
+
+// API
 const Express=require('express')
 const Server=Express()
 
