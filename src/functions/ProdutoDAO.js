19737752e93b2ebe0656acd068b85072039b5f37
N1C0L4Ssilva <nicolassipva3317@gmail.com>
2023-12-22 12:12:03 -0300

    Adicionar pasta Functions

---
+++ src/functions/ProdutoDAO.js
@@ -0,0 +1,12 @@
+exports.GET=(event, context)=>{
+    console.log(event,context)   
+}
+exports.PUT=(event, context)=>{
+
+}
+exports.POST=(event, context)=>{
+
+}
+exports.DELETE=(event, context)=>{
+
+}
